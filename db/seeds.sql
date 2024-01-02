144cc6cc610c4f0126bf4d56c8d2b3f1092bd1d7
Chris Rice <chrisrice2442@yahoo.com>
2024-01-02 14:13:21 -0500

    first part of seeds done, setting up the dept names in the table

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO departments (department_name)
+VALUES
+('Executive Board'),
+('Marketing'),
+("Human Resources"),
+('Finances'),
+('Engineering'),
+('Information Tech'),
+('Customer Relations'),
+('R & D'),
+('Legal'),
+('Maintenance');
+
