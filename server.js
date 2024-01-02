32e09640773cc39f490ef08bedb1bf68e973c870
Chris Rice <chrisrice2442@yahoo.com>
2024-01-02 14:36:33 -0500

    starting server.js file

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-const express = require('express');
-const routes = require('./routes');
+const mysql = require('mysql2');
 const inquirer = require('inquirer')
-// import sequelize connection
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'employeeTracker_db'
+  },
+  console.log('connected to db')
+).promise();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(routes);
-
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
