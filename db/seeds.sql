6280a990e177ca27a66e9bed6da86cacfcfa92a1
Chris Rice <chrisrice2442@yahoo.com>
2024-01-02 14:24:15 -0500

    simplified departments, added 1 role in each dept

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,21 @@
 INSERT INTO departments (department_name)
 VALUES
-('Executive Board'),
 ('Marketing'),
-("Human Resources"),
+('Sales'),
+('Legal'),
 ('Finances'),
 ('Engineering'),
-('Information Tech'),
-('Customer Relations'),
 ('R & D'),
-('Legal'),
-('Maintenance');
 
+INSERT INTO roles (title, salary, department_id)
+VALUES
+
+('CEO', 150000, 4),
+('Salesperson', 80000, 2),
+('Lawyer', 100000, 3),
+('Head Researcher', 85000, 6),
+('Director of Advertising', 90000, 1),
+('Assistant Engineer', 50000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
