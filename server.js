80233e1ffe4a22fa4db187bbd1cf0ed0b3916872
Chris Rice <chrisrice2442@yahoo.com>
2024-01-02 14:46:44 -0500

    working through the server file

--- server.js
+++ server.js
@@ -4,10 +4,45 @@ const inquirer = require('inquirer')
 const db = mysql.createConnection(
   {
     host: 'localhost',
+    port: 3301,
     user: 'root',
     password: '',
     database: 'employeeTracker_db'
-  },
-  console.log('connected to db')
-).promise();
+  };
 
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('connected!');
+  start();
+});
+
+function start() {
+  inquirer
+    .prompt({
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to view?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add dept',
+        'Add role',
+        'Add employee',
+        'Update employee role',
+      ],
+    
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case 'View all departments':
+          viewAllDepartments();
+          break;
+        case 'View all roles':
+          viewAllRoles();
+          break;
+        case 
+      }
+    })
+}
